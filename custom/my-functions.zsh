363fe280abafb88641803f448e3fb6ed0b8c03e1
kliu <kyle.lzy@gmail.com>
2021-03-30 15:05:36 +0000

    Updates

--- custom/my-functions.zsh
+++ custom/my-functions.zsh
@@ -2,14 +2,7 @@
 # Funny =================================
 alias busy="cat /dev/urandom | hexdump -C | grep 'ca fe'"
 
-# Develop =================================
-alias d1="export JPDA=123"
-alias d0="export JPDA="
-alias mvn-debug="export MAVEN_OPTS='-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,suspend=y,server=y'"
-alias mvn-debugoff="export MAVEN_OPTS="
-alias ungit="find . -name '.git' -exec rm -rf {} \;"
-alias gitc='git -c user.name="zhiyuliu" -c user.email="zhiyuliu@cisco.com"'
-
+# Editor =================================
 e () {
   [[ -a "$1" ]] || touch $1
   code "$@"
@@ -17,21 +10,102 @@ e () {
   #"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" "$@"
 }
 
-function zipup() {
+man_pdf () { #display man page as a PostScript PDF in Preview.app
+  man -t "$1"|open -f -a "Preview.app"
+}
+
+man_txt () { #open man page in TextMate
+  MANWIDTH=160 MANPAGER='col -bx' man $@ | e
+}
+
+# Maven  =================================
+alias mvn_debug="export MAVEN_OPTS='-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,suspend=y,server=y'"
+alias mvn_debugoff="export MAVEN_OPTS="
+
+# Git =================================
+alias git_ungit="find . -name '.git' -exec rm -rf {} \;"
+
+function git_config_cisco() {
+  git -c user.name="zhiyuliu" -c user.email="zhiyuliu@cisco.com"
+}
+
+function git_zip() {
   git archive --format zip --output $1 master
 }
 
-function gits() {
-  git $@ --config "http.proxy=socks5://$SOCKS_SERVER"
+function git_commit_search() {
+  git grep $1 $(git rev-list --all -- $2) -- $2
 }
 
-function setjdk() {
-    export JAVA_HOME=`/usr/libexec/java_home -v $@`
+function git_commit_diff() {
+  git difftool $1^! 
 }
 
-alias lsjava='/usr/libexec/java_home -V 2>&1 | grep ", " | cut -d , -f 1 | cut -d : -f 3 | tr -d " "'
+# System  =================================
+alias f="open -a Finder ./"
+alias o="open -a"
+alias mkdir='mkdir -p -v'
+alias grep="grep --color=auto -i -H -n"
+alias file_find="find . -name"
+alias file_path="pwd | pbcopy"
+alias file_size="du -sh"
+alias ls="ls -GlFh"
+alias lt='echo "------Newest--" && ls -At1 -GlFh && echo "------Oldest--"'
+alias ltr='echo "------Oldest--" && ls -Art1 -GlFh && echo "------Newest--"'
 
-# Sync =================================
+pfd() {
+  osascript 2>/dev/null -e '
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell'
+}
+
+file_goto() {
+  cd "$(pfd)"
+}
+
+file_extract () { #unarchive various compression formats based on extension
+        if [ -f $1 ] ; then
+                case $1 in
+                        *.tar.bz2)          tar xjf $1       ;;
+                        *.tar.gz)           tar xzf $1       ;;
+                        *.bz2)              bunzip2 $1       ;;
+                        *.rar)              rar x $1         ;;
+                        *.gz)               gunzip $1        ;;
+                        *.tar)              tar xf $1        ;;
+                        *.tbz2)             tar xjf $1       ;;
+                        *.tgz)              tar xzf $1       ;;
+                        *.zip)              unzip $1         ;;
+                        *.Z)                uncompress $1    ;;
+                        *.dmg)              hdiutil mount $1 ;;
+                        *)                  echo "'$1' cannot be extracted via extract()" ;;
+                esac
+        else
+                echo "'$1' is not a valid file"
+        fi
+}
+
+md5check() { md5sum "$1" | grep "$2";}
+
+# Security =================================
+alias key_pub="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+key_safe() {
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/*
+  chmod 400 ~/.ssh/*.pem
+}
+
+# Networking =================================
+alias web_get_site="wget --random-wait -r -p -e robots=off -U mozilla"
+alias web_get='curl -O'
+alias list_port="lsof -P -i -n"
+alias list_ip="curl ifconfig.me"
+alias scp="scp -v -p -r -C"
+alias aria='aria2c --conf-path="/Users/lzy/.dotfiles/aria2/aria2.conf" -D'
+alias ariaui='open /Users/lzy/.dotfiles/aria2/webui-aria2/docs/index.html'
+# alias skype2='sudo /Applications/Skype.app/Contents/MacOS/Skype'
+alias skype2='open -na /Applications/Skype.app --args --secondary --datapath="/Users/$(whoami)/Library/Application\ Support/Skype2"'
 
 function push() {
     local POSITIONAL=()
@@ -86,159 +160,11 @@ function push() {
     eval $command
 }
 
-# System  =================================
-alias f="open -a Finder ./"
-alias o="open -a"
-alias fname="find . -name"
-alias grep="grep --color=auto -i -H -n"
-alias mkdir='mkdir -p -v'
-alias pc="pwd | pbcopy"
-alias sz="du -sh"
-if [[ $OS_TYPE == 'osx' ]]; then
-	alias ls="ls -GlFh"
-	alias lt='echo "------Newest--" && ls -At1 -GlFh && echo "------Oldest--"'
-	alias ltr='echo "------Oldest--" && ls -Art1 -GlFh && echo "------Newest--"'
-elif [[ $OS_TYPE == 'linux' ]]; then
-	alias ls="ls -lFh --color=auto"
-	alias lt='echo "------Newest--" && ls -At1 -lFh --color=auto && echo "------Oldest--"'
-	alias ltr='echo "------Oldest--" && ls -Art1 -lFh --color=auto && echo "------Newest--"'
-fi
-
-console () {
-  if [[ $# > 0 ]]; then
-    query=$(echo "$*"|tr -s ' ' '|')
-    tail -f /var/log/system.log|grep -i --color=auto -E "$query"
-  else
-    tail -f /var/log/system.log
-  fi
-}
-
-fk () {
-  IFS=$'\n'
-  PS3='Kill which process? (1 to cancel): '
-  select OPT in "Cancel" $(fp $1); do
-    if [ $OPT != "Cancel" ]; then
-      kill $(echo $OPT|awk '{print $NF}')
-    fi
-    break
-  done
-  unset IFS
-}
-
-manpdf () { #display man page as a PostScript PDF in Preview.app
-  man -t "$1"|open -f -a "Preview.app"
-}
-
-mantxt () { #open man page in TextMate
-  MANWIDTH=160 MANPAGER='col -bx' man $@ | e
-}
-
-function sr {
-    find . -type f -exec sed -i '' s/$1/$2/g {} +
-}
-
-# File =================================
-extract () { #unarchive various compression formats based on extension
-        if [ -f $1 ] ; then
-                case $1 in
-                        *.tar.bz2)          tar xjf $1       ;;
-                        *.tar.gz)           tar xzf $1       ;;
-                        *.bz2)              bunzip2 $1       ;;
-                        *.rar)              rar x $1         ;;
-                        *.gz)               gunzip $1        ;;
-                        *.tar)              tar xf $1        ;;
-                        *.tbz2)             tar xjf $1       ;;
-                        *.tgz)              tar xzf $1       ;;
-                        *.zip)              unzip $1         ;;
-                        *.Z)                uncompress $1    ;;
-                        *.dmg)              hdiutil mount $1 ;;
-                        *)                  echo "'$1' cannot be extracted via extract()" ;;
-                esac
-        else
-                echo "'$1' is not a valid file"
-        fi
-}
-
-md5check() { md5sum "$1" | grep "$2";}
-
-# Security =================================
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-
-safekey () {
-  chmod 700 ~/.ssh
-  chmod 600 ~/.ssh/*
-  chmod 400 ~/.ssh/*.pem
-}
-
-# Networking =================================
-alias websiteget="wget --random-wait -r -p -e robots=off -U mozilla"
-alias listen="lsof -P -i -n"
-alias ip="curl icanhazip.com"
-alias scp="scp -v -p -r -C"
-alias curls='curl --socks5-hostname $SOCKS_SERVER'
-alias cget='curl -O'
-alias cgets='curls -O'
-alias sshs="ssh -o 'ProxyCommand=nc -X 5 -x 127.0.0.1:1080 %h %p' -o 'ServerAliveInterval=10'"
-alias proxy='proxychains4'
-alias aria='aria2c --conf-path="/Users/lzy/.dotfiles/aria2/aria2.conf" -D'
-alias ariaui='open /Users/lzy/.dotfiles/aria2/webui-aria2/docs/index.html'
-
-rrestore() {
-  if [[ $# == 2 ]]; then
-    rsync -avz --progress -e ssh $1:$2 .
-  else
-    echo "Usage: rbak host path"
-  fi
-}
-
-rbackup() {
-  if [[ $# == 2 ]]; then
-    rsync -avz --progress -e ssh . $1:$2
-  else
-    echo "Usage: rbak host path"
-  fi
-}
-
-function serve() {
+function web_serve() {
   python3 -m http.server
 }
 
-call () {
-  number=`echo "$1"`
-  if [[ "$number" =~ ^'#' ]]; then
-    number=`echo "${number:1}"`
-    if [[ "$number" =~ ^'+' ]]; then
-      number=`echo "$number"`
-    else
-      number=`echo "+86${number}"`
-    fi
-  fi
-  # number=`echo $1|sed 's/[\(\)\+ \-]//g'|sed 's/^1//'|sed 's/^/+/'`
-  osascript -e "tell application \"Skype\" to send command \"CALL $number\" script name \"CLIDIALER\""
-}
-
-jekyll_deploy() {
-  jekyll build
-  if [ -d ".git" ] ; then
-    git add --all .
-    git commit -m "Update site"
-    git push origin source
-    cd _site
-    git add --all .
-    git commit -m "Update site"
-    git push origin master
-    cd ..
-  else
-    rsync -rlptDvz --delete --progress --exclude '_site' . root@helloworld.me:/mnt/data/develop/kliu.helloworld.me
-    rsync -rlptDvz --delete --progress _site/  root@helloworld.me:/mnt/data/docs
-  fi
-}
-
-jekyll_newpost() {
-  touch _posts/$1.md
-  mkdir downloads/$1
-}
-
+# Blog  =================================
 hexo_draft() {
   cd $BLOG_HOME
   POST=$(hexo new draft $1 | tail -n1 | sed 's/INFO  Created: //g')
@@ -272,25 +198,7 @@ hexo_deploy() {
   cd -
 }
 
-pfd() {
-  osascript 2>/dev/null -e '
-    tell application "Finder"
-      return POSIX path of (target of window 1 as alias)
-    end tell'
-}
-
-cdf() {
-  cd "$(pfd)"
-}
-
-marked() {
-  if [ $1 ]; then
-    open -a "Marked" $1;
-  else
-    open -a "Marked";
-  fi
-}
-
+# Kotlin  =================================
 function kr() {
   echo Compiling, please wait...
   kotlinc $1 -include-runtime -d $TMPDIR\me.helloworld.kotlinr-out.jar
